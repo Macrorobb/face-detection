fa637058a4613ee09e01b07205fd93daf89540a0
Macrorobb <macrorobb@outlook.com>
2021-12-13 00:28:54 +0700

    update test face-api completed.

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import NewPost from './components/NewPost';
+
+function App() {
+  const [file, setFile] = useState();
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    const getImage = () => {
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+        setImage({
+          url: img.src,
+          width: img.width,
+          height: img.height,
+        });
+      };
+    };
+
+    file && getImage();
+  }, [file]);
+
+  return (
+    <div>
+      <Navbar />
+      {image ? (
+        <NewPost image={image} />
+      ) : (
+        <div className="newPostCard">
+          <div className="addPost">
+            <img
+              src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+              alt=""
+              className="avatar"
+            />
+            <div className="postForm">
+              <input
+                type="text"
+                placeholder="What's on your mind?"
+                className="postInput"
+              />
+              <label htmlFor="file">
+                <img
+                  className="addImg"
+                  src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                  alt=""
+                />
+                <img
+                  className="addImg"
+                  src="https://icon-library.com/images/maps-icon-png/maps-icon-png-5.jpg"
+                  alt=""
+                />
+                <img
+                  className="addImg"
+                  src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
+                  alt=""
+                />
+                <button>Send</button>
+              </label>
+              <input
+                onChange={(e) => setFile(e.target.files[0])}
+                id="file"
+                style={{ display: "none" }}
+                type="file"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <span>JobbexecSocial</span>
+    </div>
+  );
+};
+
+export default Navbar;
